fac91732484fa3e7ae3c75674e06723d48c08753
Jeff Winkler <jeff.a.winkler@gmail.com>
2017-11-23 08:05:45 -0600

    added components

---
+++ src/UserInput/Userinput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Userinput.css';
+
+const userinput = (props) => {
+    return(
+        <div className='Userinput'>
+            <input type='text' onChange={}/>
+        </div>
+    )
+}
+
+export default userinput;
---
+++ src/Useroutput/Userinput.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Useroutput.css';
+
+const userinput = (props) => {
+    return(
+        <div className='Useroutput'>
+            <p>The big brown bear<p/>
+            <P>Jumped over the lazy dog<p/>
+        </div>
+    )
+}
